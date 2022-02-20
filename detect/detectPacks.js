ca6915e7065c51f5d01999a437e2080b6100ac5e
PiSaucer <33438654+PiSaucer@users.noreply.github.com>
2022-02-20 17:22:24 -0600

    detectPack Test

---
+++ detect/detectPacks.js
@@ -0,0 +1,26 @@
+function onMyfileChange(fileInput) {
+    document.getElementById("convertedPath").value = "";
+    if (fileInput.files[0] == undefined) {
+        return;
+    }
+    var filename = fileInput.files[0].name;
+    var reader = new FileReader();
+    reader.onload = function(ev) {
+        JSZip.loadAsync(ev.target.result).then(function(zip) {
+            zip.forEach(function(relativePath, zipEntry) {
+                if (zipEntry.name == "PACKS.txt") {
+                    zip.file(zipEntry.name).async("string").then(function(result) {
+                        document.getElementById("convertedPath").value = result;
+                    });
+                }
+            });
+            document.getElementById("convertedPath").value = document.getElementById("convertedPath").value.replaceAll(/undefined/g, "");
+        }).catch(function(err) {
+            console.error("Failed to open", filename, " as ZIP file:", err);
+        })
+    };
+    reader.onerror = function(err) {
+        console.error("Failed to read file", err);
+    }
+    reader.readAsArrayBuffer(fileInput.files[0]);
+}
