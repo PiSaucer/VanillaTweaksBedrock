c97ad72f7e0f29ec92058f8b424cac30aa218347
PiSaucer <33438654+PiSaucer@users.noreply.github.com>
2022-07-23 09:45:50 -0500

    Created enablePacks Button for test

---
+++ detect/enablePacks.js
@@ -0,0 +1,13 @@
+function enablePacks() {
+    let packsString = document.getElementById("convertedPath").value;
+    let packsArray = packsString.split("\n");
+    packsArray.pop();
+    packsArray.pop();
+    packsArray.forEach(element => {
+        if (document.getElementById(element)) {
+            document.getElementById(element).checked = true;
+        } else {
+            console.error("Unable to find " + element + " id in DOM");
+        }
+    });
+}
